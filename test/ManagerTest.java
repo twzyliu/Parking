3f48284c08ae3fc9075d0ba0562dc662d55ac4b9
Zhiyong Liu <zyongliu@thoughtworks.com>
2016-11-09 20:20:17 +0800

    Refecting Manager Class and Test

--- test/ManagerTest.java
+++ test/ManagerTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -11,29 +12,37 @@ import static org.junit.Assert.assertThat;
 public class ManagerTest {
 
     public static final int CAPACITY = 2;
+    private Car car;
+    private Parking fullParking;
+
+    @Before
+    public void setUp() throws Exception {
+        car = new Car(0);
+        fullParking = new Parking(CAPACITY);
+        fullParking.carComeIn(car);
+        fullParking.carComeIn(car);
+    }
 
     @Test
     public void manager_should_find_parking_which_has_more_space() throws Exception {
         Parking parking1 = new Parking(CAPACITY);
         Parking parking2 = new Parking(CAPACITY);
-        Parking parking3 = new Parking(CAPACITY);
-        parking1.carComeIn(new Car(0));
-        parking2.carComeIn(new Car(1));
+        parking1.carComeIn(car);
         ArrayList<Parking> parkList = new ArrayList<>();
         parkList.add(parking1);
         parkList.add(parking2);
-        parkList.add(parking3);
+        parkList.add(fullParking);
         Manager manager = new Manager(parkList);
         assertThat(manager.canPark(), is(true));
-        assertThat(manager.findParking(), is(parking3));
+        assertThat(manager.findParking(), is(parking2));
     }
 
     @Test
     public void manager_cannot_park_car_when_all_parking_are_full() throws Exception {
         ArrayList<Parking> parkList = new ArrayList<>();
-        parkList.add(new Parking(CAPACITY));
-        parkList.add(new Parking(CAPACITY));
-        parkList.add(new Parking(CAPACITY));
+        parkList.add(fullParking);
+        parkList.add(fullParking);
+        parkList.add(fullParking);
         Manager manager = new Manager(parkList);
         assertThat(manager.canPark(), is(false));
     }
